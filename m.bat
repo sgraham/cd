32db8f8a51e3bd04c749a471152c8cd343a2798a
Scott Graham <scottmg@chromium.org>
2017-01-02 16:26:21 -0800

    fix

--- m.bat
+++ m.bat
@@ -1,4 +1,4 @@
 @echo off
 call cl /nologo /Zi crash.cc
-call cl /D_HAS_EXCEPTIONS=0 /wd4530 /nologo /Zi /W4 /WX /Ithird_party/include/lldb/API /Ithird_party/include main.cc /link third_party\lib\liblldb.lib
-call cd crash
+call cl /D_HAS_EXCEPTIONS=0 /wd4530 /nologo /Zi /W4 /WX /Ithird_party/include/lldb/API /Ithird_party/include main.cc /link third_party\lib\liblldb.lib /out:cd.exe
+.\cd crash
